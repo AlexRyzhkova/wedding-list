9c6076a236e75640c97ea81cc25d3fa28d8eef18
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-08-28 13:17:37 +0200

    Add conntection server.js to react, create lib and add route

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const todos = require("./lib/routes/todos");
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.use(express.json());
+
+app.use("/api/todos", todos);
+
+app.use(express.static(path.join(__dirname, "build/")));
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "build/index.html"));
 });
 
-app.listen(3003, function () {
+app.listen(3003, () => {
   console.log("listening on 3003");
 });
